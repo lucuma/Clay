cfcc685f79ad4a91ce89416380499374653721db
Juan Pablo Scaletti <juanpablo@lucumalabs.com>
2011-11-15 14:43:14 -0500

    test_make

--- tests/test_manage.py
+++ tests/test_manage.py
@@ -15,5 +15,14 @@ def test_get_settings():
     assert 'views_list' in settings
 
 
-# def test_make():
-#     execute('clay make')
+def test_make():
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    build_dir = os.path.join(cwd, 'build')
+    try:
+        os.remove(build_dir)
+    except OSError:
+        pass
+    
+    execute('clay', 'make')
+
+    assert os.path.isdir(build_dir)
