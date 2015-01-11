f14fa0ed82dd9a33caa93c4d51da721d9f703cea
Juan-Pablo Scaletti <juanpablo@lucumalabs.com>
2015-01-11 15:01:40 -0500

    test toc folders_first

--- tests/test_tg_toc.py
+++ tests/test_tg_toc.py
@@ -101,3 +101,45 @@ def test_toc_render_maxdepth1():
     html = toc(maxdepth=1)
     print(html)
     assert expected == html
+
+
+def test_toc_render_maxdepthplus():
+    toc = ToC(CLAY_SOURCE_PATH, filter=FILTER)
+    assert toc(maxdepth=2) == toc(maxdepth=9999)
+
+
+def test_toc_render_folders_first():
+    expected = """<ul class="toc">
+    <li><span>markdown_ext</span>
+        <ul class="toc">
+            <li><a href="/markdown_ext/__init__.py">__init__.py</a></li>
+            <li><a href="/markdown_ext/jinja.py">jinja.py</a></li>
+            <li><a href="/markdown_ext/md_admonition.py">md_admonition.py</a></li>
+            <li><a href="/markdown_ext/md_delinsmark.py">md_delinsmark.py</a></li>
+            <li><a href="/markdown_ext/md_fencedcode.py">md_fencedcode.py</a></li>
+            <li><a href="/markdown_ext/md_superscript.py">md_superscript.py</a></li>
+            <li><a href="/markdown_ext/render.py">render.py</a></li>
+        </ul>
+    </li>
+    <li><span>source</span>
+        <ul class="toc">
+            <li><a href="/source/_index.html">_index.html</a></li>
+            <li><a href="/source/_index.txt">_index.txt</a></li>
+            <li><a href="/source/_notfound.html">_notfound.html</a></li>
+        </ul>
+    </li>
+    <li><a href="/__init__.py">__init__.py</a></li>
+    <li><a href="/helpers.py">helpers.py</a></li>
+    <li><a href="/jinja_includewith.py">jinja_includewith.py</a></li>
+    <li><a href="/main.py">main.py</a></li>
+    <li><a href="/manage.py">manage.py</a></li>
+    <li><a href="/server.py">server.py</a></li>
+    <li><a href="/static.py">static.py</a></li>
+    <li><a href="/tglobals.py">tglobals.py</a></li>
+    <li><a href="/wsgiapp.py">wsgiapp.py</a></li>
+</ul>"""
+
+    toc = ToC(CLAY_SOURCE_PATH, filter=FILTER)
+    html = toc(maxdepth=1, folders_first=True)
+    print(html)
+    assert expected == html
